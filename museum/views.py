90ae2e21d13a9a591699a8024d98fe68be84588a
Nick Karamoff <nikita.karamoff@gmail.com>
2018-07-09 12:08:40 +0300

    Create beautiful placeholder

--- museum/views.py
+++ museum/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This will be a museum eventually")
+    return render(
+        request,
+        'index.html'
+    )

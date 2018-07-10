5e1b19b27e556d59ec72a213bc7ec6e3d31344ee
Nick Karamoff <nikita.karamoff@gmail.com>
2018-07-10 14:01:43 +0300

    Add __str__ methods to models

--- museum/models.py
+++ museum/models.py
@@ -4,12 +4,18 @@ from django.db import models
 class HallModel(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name + ". Exhibits: " + ExhibitModel.objects.filter(hall_id=self.id)
+
 
 class ExhibitModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateField()
     hall = models.ForeignKey(HallModel, related_name="exhibits", on_delete="RESTRICT")
+
+    def __str__(self):
+        return self.name
 
 
 class PictureModel(models.Model):

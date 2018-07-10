9de81eadd6a17124edbf30e5c758718bac0034e5
Nick Karamoff <nikita.karamoff@gmail.com>
2018-07-10 13:52:52 +0300

    Create models for halls, exhibits and pictures

--- museum/models.py
+++ museum/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class HallModel(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ExhibitModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    hall = models.ForeignKey(HallModel, related_name="exhibits", on_delete="RESTRICT")
+
+
+class PictureModel(models.Model):
+    image = models.ImageField()
+    exhibit = models.ForeignKey(ExhibitModel, related_name="images", on_delete="NULL")

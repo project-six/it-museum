88d98fb0e36466466db063194848714f4708022a
Nick Karamoff <nikita.karamoff@gmail.com>
2018-07-10 17:48:45 +0300

    Localize exhibit model

--- museum/models.py
+++ museum/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class HallModel(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Название")
 
     def __str__(self):
         return self.name
@@ -13,11 +13,11 @@ class HallModel(models.Model):
 
 
 class ExhibitModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    epoch = models.CharField(max_length=32)
-    country = models.CharField(max_length=2)
-    hall = models.ForeignKey(HallModel, related_name="exhibits", on_delete="RESTRICT")
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    epoch = models.CharField(max_length=32, verbose_name="Год(-ы)")
+    country = models.CharField(max_length=2, verbose_name="Страна")
+    hall = models.ForeignKey(HallModel, related_name="exhibits", on_delete="RESTRICT", verbose_name="Зал")
 
     def __str__(self):
         return self.name
@@ -28,8 +28,8 @@ class ExhibitModel(models.Model):
 
 
 class PictureModel(models.Model):
-    image = models.ImageField()
-    exhibit = models.ForeignKey(ExhibitModel, related_name="images", on_delete="NULL")
+    image = models.ImageField(verbose_name="Изображение")
+    exhibit = models.ForeignKey(ExhibitModel, related_name="images", on_delete="NULL", verbose_name="Экспонат")
 
     class Meta:
         verbose_name = "Изображение"

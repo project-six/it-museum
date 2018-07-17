ba5d7d5e6f5e39bdbc79af4ae91d9008004b249e
Nick Karamoff <nikita.karamoff@gmail.com>
2018-07-17 15:21:55 +0300

    Initialize watson

--- museum/apps.py
+++ museum/apps.py
@@ -1,6 +1,11 @@
+from watson import search as watson
 from django.apps import AppConfig
 
 
 class MuseumConfig(AppConfig):
     name = 'museum'
     verbose_name = "IT-Museum"
+
+    def ready(self):
+        exhibit = self.get_model('Exhibit')
+        watson.register(exhibit, fields=('name', 'description'))

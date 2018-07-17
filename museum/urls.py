215bb5c1881cefa159713c26cf164e565806e621
Nick Karamoff <nikita.karamoff@gmail.com>
2018-07-18 01:38:30 +0300

    Exhibits now can be accessed via links

--- museum/urls.py
+++ museum/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('halls/', views.hall_list, name='hall_list'),
-    re_path(r'^halls/(\d+)/$', views.hall, name='hall'),
-    re_path(r'^exhibits/(\d+)/$', views.exhibit, name='exhibit'),
+    re_path(r'^halls/(?P<hall_number>\d+)(?:/exhibit/(?P<exh_id>\d+))?/$', views.hall, name='hall'),
+    re_path(r'^exhibit_data/(\d+)/$', views.exhibit_data, name='exhibit_data'),
     path('propose/', views.propose, name='propose'),
     path('search/', views.search, name='search')
 ]
